59b917a6e05f6e990a8b269045106547a97e8a4e
Mayur Dushetwar <114808463+mdushetwar@users.noreply.github.com>
2023-03-08 22:36:03 +0530

    Flask app development

--- app.py
+++ app.py
@@ -1,15 +1,41 @@
 # Importing main Libraries
 from flask import Flask, render_template, jsonify, request
-import pandas
-import numpy
+import pandas as pd
+import numpy as np
 import pickle
 
 # Creating Flask app
 app= Flask(__name__)
 
+
+# Loading model and preprocessor
+
+model = pickle.load(open('model.pkl', 'rb'))
+scaler= pickle.load(open('scaler.pkl', 'rb'))
+
 @app.route('/')
 def Home():
     return render_template('index.html')
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    # Getting the form data from the request object
+    months_since_last_donation = int(request.form.get('months_since_last_donation'))
+    months_since_first_donation = int(request.form.get('months_since_first_donation'))
+    number_of_donations = int(request.form.get('number_of_donations'))
+
+    features= np.array([months_since_last_donation, months_since_first_donation, number_of_donations])
+
+    # Preprocessing
+    feature_list= list(features)
+    feature_sqrt= [np.sqrt(x) for x in feature_list]
+    feature_scaled=scaler.transform(feature_sqrt)
+    feature_transformed= np.array(feature_scaled).reshape(1,-1)
+
+    # prediction
+    prediction= (model.predict_proba(feature_transformed)[:, 1] > 0.6).astype(int)
+
+    return jsonify({'prediction': prediction.tolist()})
 
 
 if __name__ == '__main__':
